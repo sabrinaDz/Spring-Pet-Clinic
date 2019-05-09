c85c94f13001eae7ac8b67f1f6e43b249ae0b859
sabrinaDz <sabrinasahli@live.fr>
2019-05-09 12:55:25 +0200

    #

--- Pet Clinic Demo/petclinicdata/src/main/java/dz/springframework/petclinic/demo/services/CrudService.java
+++ Pet Clinic Demo/petclinicdata/src/main/java/dz/springframework/petclinic/demo/services/CrudService.java
@@ -2,11 +2,11 @@ package dz.springframework.petclinic.demo.services;
 
 import java.util.Set;
 
-import dz.springframework.petclinic.demo.model.Owner;
+ 
 
 public interface CrudService<T,ID> {
 
-	 
+	
 	T findById(Long id);
 	Set<T>findAll();
 	T save(T object);

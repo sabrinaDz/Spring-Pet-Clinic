c92f3b1488ce4eec892743bf4056b597555a59db
sabrinaDz <sabrinasahli@live.fr>
2019-07-23 16:15:38 +0200

    #31 create Speciality Service

---
+++ Pet Clinic Demo/petclinicdata/src/main/java/dz/springframework/petclinic/demo/map/SpecialityMapService.java
@@ -0,0 +1,42 @@
+package dz.springframework.petclinic.demo.map;
+
+import java.util.Set;
+import dz.springframework.petclinic.demo.model.Speciality;
+import dz.springframework.petclinic.demo.services.SpecialityService;
+
+public class SpecialityMapService extends AbstractMapService<Speciality,Long> 
+implements SpecialityService{
+
+	public Set<Speciality>findAll(){
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality speciality) {
+		// TODO Auto-generated method stub
+		return super.save(speciality);
+	}
+
+	@Override
+	public void delete(Speciality speciality) {
+		// TODO Auto-generated method stub
+		super.delete(speciality);
+	}
+
+	@Override
+	public void deleteBydId(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+
+
+
+
+}
---
+++ Pet Clinic Demo/petclinicdata/src/main/java/dz/springframework/petclinic/demo/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package dz.springframework.petclinic.demo.services;
+
+import dz.springframework.petclinic.demo.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long>{
+
+}

a49591e934dbec86bbe7f787374f7fb4b6e237ee
sabrinaDz <sabrinasahli@live.fr>
2019-07-23 09:30:43 +0200

    #28 Create PetType Map Service

---
+++ Pet Clinic Demo/petclinicdata/src/main/java/dz/springframework/petclinic/demo/map/PetTypeServiceMap.java
@@ -0,0 +1,46 @@
+package dz.springframework.petclinic.demo.map;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import dz.springframework.petclinic.demo.model.Pet;
+import dz.springframework.petclinic.demo.model.PetType;
+import dz.springframework.petclinic.demo.services.PetService;
+import dz.springframework.petclinic.demo.services.PetTypeService;
+
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long>
+implements PetTypeService {
+
+	public Set<PetType>findAll(){
+		 return super.findAll();
+	 }
+	 
+	@Override
+	public PetType findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType petType) {
+		// TODO Auto-generated method stub
+		return super.save(petType);
+	}
+
+	@Override
+	public void delete(PetType petType) {
+		// TODO Auto-generated method stub
+		super.delete(petType);
+	}
+
+	@Override
+	public void deleteBydId(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+}
---
+++ Pet Clinic Demo/petclinicdata/src/main/java/dz/springframework/petclinic/demo/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package dz.springframework.petclinic.demo.services;
+
+import dz.springframework.petclinic.demo.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
